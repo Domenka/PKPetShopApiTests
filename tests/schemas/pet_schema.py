9b88838000b4b137d321f76a1bc713f0bc9e98ea
Polina <123462814+Domenka@users.noreply.github.com>
2026-05-16 19:09:47 +0400

    deleted comment

--- tests/schemas/pet_schema.py
+++ tests/schemas/pet_schema.py
@@ -47,22 +47,3 @@ PET_SCHEMA = {
     "required": ["id", "name", "status", "photoUrls"],
     "additionalProperties": False
 }
-
-# {
-#   "id": 10,
-#   "name": "doggie",
-#   "category": {
-#     "id": 1,
-#     "name": "Dogs"
-#   },
-#   "photoUrls": [
-#     "string"
-#   ],
-#   "tags": [
-#     {
-#       "id": 0,
-#       "name": "string"
-#     }
-#   ],
-#   "status": "available"
-# }
